47e9b3f93a8cfdb7a05b3f027120622fef96e7d2
painye <1850384745@qq.com>
2022-03-22 20:32:52 +0800

    将consumer注册到注册zhongxin

--- cloud-consumer-order80/src/main/java/com/yp/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/yp/springcloud/OrderMain80.java
@@ -6,6 +6,7 @@ package com.yp.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @ClassName : com.yp.springcloud.OrderMain80
@@ -15,6 +16,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
--- cloud-provider-payment8001/src/main/java/com/yp/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/yp/springcloud/PaymentMain8001.java
@@ -16,7 +16,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
