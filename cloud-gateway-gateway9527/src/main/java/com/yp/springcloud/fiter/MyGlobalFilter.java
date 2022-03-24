fc8be48c3fcf948525eecbaa01a9294b0909c8df
painye <1850384745@qq.com>
2022-03-24 19:01:31 +0800

    init yml

---
+++ cloud-gateway-gateway9527/src/main/java/com/yp/springcloud/fiter/MyGlobalFilter.java
@@ -0,0 +1,14 @@
+package com.yp.springcloud.fiter;
+/**
+ * @author pan
+ * @date 2022/3/24 18:07
+ */
+
+/**
+ * @ClassName : com.yp.springcloud.fiter.MyGlobalFilter
+ * @Description : 类描述
+ * @author pan
+ * @date 2022/3/24 18:07
+ */
+public class MyGlobalFilter {
+}
